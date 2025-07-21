0ea6191f4ea2478af1003e4f3d00504f0b2e3814
e-mandy <andymfrd02@gmail.com>
2025-07-21 12:30:08 +0100

    Récupération de la branche main

--- controllers/factureController.py
+++
@@ -1,8 +0,0 @@
-from models.model import Model
-from models.client import ClientCodeError
-from models.client import Client
-from services import 
-
-def insert_facture(code_client):
-    if(Client._findEntry("Clients.xlsx", "code_client", code_client)):
-        
